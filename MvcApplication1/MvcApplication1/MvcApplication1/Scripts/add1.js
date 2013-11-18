17ea1bfb32c21af2f5432b1945632f72d202df31
wanglifeng <wang.lifeng@careerbuilder.com>
2013-11-18 14:49:33 +0800

    Add new Clothes through Ajax method

--- MvcApplication1/MvcApplication1/MvcApplication1/Scripts/add1.js
+++ MvcApplication1/MvcApplication1/MvcApplication1/Scripts/add1.js
@@ -1,15 +1,30 @@
 $(document).ready(function () {
     $('#btnAddUser1').click(function () {
-        $.post('/ClothInfo/Add', { 'chengfen': $('#chengfen').val() }, function (cloth) {
+        var formData = {
+            'chengfen': $('#chengfen').val(),
+            'houdu': $('#houdu').val(),
+            'touqixing': $('#touqixing').val(),
+            'xuanchuixing': $('#xuanchuixing').val(),
+            'yiling': $('#yiling').val(),
+            'xiukou': $('#xiukou').val(),
+            'dibai': $('#dibai').val(),
+            'qitakaikou': $('#qitakaikou').val(),
+            'haoxing': $('#haoxing').val(),
+            'lingwei': $('#lingwei').val(),
+            'xiukouwei': $('#xiukouwei').val(),
+            'xiabaiwei': $('#xiabaiwei').val(),
+            'qita': $('#qita').val()
+        };
+        $.post('/ClothInfo/Add', formData, function (data) {
             if (data == true) {
                 alert('ok');
                 //$('#btnClose').click();
-               // document.location.reload();
+                // document.location.reload();
             }
             else {
                 alert('wrong' + cloth);
             }
         }).fail(function () { alert('wrong'); });
-        bindTdCloseIcon();
+        //bindTdCloseIcon();
     });
 });
